f05ce44d750594878e6975aa0006c24326f49a8c
Phlam Sicusa <sicusa@gilatod.art>
2023-08-08 18:42:36 +0800

    add support for impure commands

--- Sia/Commands/PropertyCommand.cs
+++ Sia/Commands/PropertyCommand.cs
@@ -1,9 +1,27 @@
 namespace Sia;
 
+public abstract class ImpurePropertyCommand<TCommand, TTarget, TValue>
+    : SingleValuePooledEvent<TCommand, TValue>, ICommand<TTarget>
+    where TCommand : ICommand<TTarget>, new()
+    where TTarget : notnull
+{
+    public abstract void Execute(World<TTarget> world, in TTarget target);
+}
+
+public abstract class ImpurePropertyCommand<TCommand, TValue>
+    : ImpurePropertyCommand<TCommand, EntityRef, TValue>, ICommand
+    where TCommand : ICommand<EntityRef>, new()
+{
+}
+
 public abstract class PropertyCommand<TCommand, TTarget, TValue>
     : SingleValuePooledEvent<TCommand, TValue>, ICommand<TTarget>
     where TCommand : ICommand<TTarget>, new()
+    where TTarget : notnull
 {
+    public void Execute(World<TTarget> world, in TTarget target)
+        => Execute(target);
+    
     public abstract void Execute(in TTarget target);
 }
 
