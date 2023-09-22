689c3ecc527f08cdcba1be03cbc3229feb19f416
Phlam Sicusa <sicusa@gilatod.art>
2023-09-22 13:52:05 +0800

    remove default implementations for ISystem

--- Sia/Systems/Interfaces/ISystem.cs
+++ Sia/Systems/Interfaces/ISystem.cs
@@ -2,24 +2,22 @@ namespace Sia;
 
 public interface ISystem
 {
-    ISystemUnion? Children => null;
-    ISystemUnion? Dependencies => null;
-    IEntityMatcher? Matcher => null;
-    IEventUnion? Trigger => null;
-    IEventUnion? Filter => null;
+    ISystemUnion? Children { get; }
+    ISystemUnion? Dependencies { get; }
+    IEntityMatcher? Matcher { get; }
+    IEventUnion? Trigger { get; }
+    IEventUnion? Filter { get; }
 
-    void Initialize(World world, Scheduler scheduler) {}
-    void Uninitialize(World world, Scheduler scheduler) {}
+    void Initialize(World world, Scheduler scheduler);
+    void Uninitialize(World world, Scheduler scheduler);
 
-    void BeforeExecute(World world, Scheduler scheduler) {}
-    void AfterExecute(World world, Scheduler scheduler) {}
-    void Execute(World world, Scheduler scheduler, in EntityRef entity) {}
+    void BeforeExecute(World world, Scheduler scheduler);
+    void AfterExecute(World world, Scheduler scheduler);
+    void Execute(World world, Scheduler scheduler, in EntityRef entity);
 
     bool OnTriggerEvent<TEvent>(World world, Scheduler scheduler, in EntityRef entity, in TEvent e)
-        where TEvent : IEvent
-        => true;
+        where TEvent : IEvent;
 
     bool OnFilterEvent<TEvent>(World world, Scheduler scheduler, in EntityRef entity, in TEvent e)
-        where TEvent : IEvent
-        => true;
+        where TEvent : IEvent;
 }
