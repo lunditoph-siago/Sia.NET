d54028bd82569cf9c67f4fa6e2ccca65004ad7ec
Phlam Sicusa <sicusa@gilatod.art>
2023-09-22 14:31:12 +0800

    add sealed tag to HOEvents

--- Sia/Events/HigherOrder/HOEvents.cs
+++ Sia/Events/HigherOrder/HOEvents.cs
@@ -4,19 +4,19 @@ using System;
 
 public static class HOEvents
 {
-    public class Cancel<TEvent> : SingletonEvent<Cancel<TEvent>>, ICancelEvent
+    public sealed class Cancel<TEvent> : SingletonEvent<Cancel<TEvent>>, ICancelEvent
         where TEvent : ICancellableEvent
     {
         public Type InnerEventType => typeof(TEvent);
     }
 
-    public class Pause<TEvent> : SingletonEvent<Pause<TEvent>>, IPauseEvent
+    public sealed class Pause<TEvent> : SingletonEvent<Pause<TEvent>>, IPauseEvent
         where TEvent : IPausableEvent
     {
         public Type InnerEventType => typeof(TEvent);
     }
 
-    public class Resume<TEvent> : SingletonEvent<Resume<TEvent>>, IResumeEvent
+    public sealed class Resume<TEvent> : SingletonEvent<Resume<TEvent>>, IResumeEvent
         where TEvent : IPausableEvent
     {
         public Type InnerEventType => typeof(TEvent);
