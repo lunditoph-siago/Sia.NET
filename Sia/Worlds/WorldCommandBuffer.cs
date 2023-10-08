43ab8b2921d89984ac5f818e7776a4c419832973
Phlam Sicusa <sicusa@gilatod.art>
2023-10-08 22:50:20 +0800

    support custom action for WorldCommandBuffer

--- Sia/Worlds/WorldCommandBuffer.cs
+++ Sia/Worlds/WorldCommandBuffer.cs
@@ -6,30 +6,76 @@ namespace Sia;
 public class WorldCommandBuffer<TCommand>
     where TCommand : ICommand
 {
-    private interface ISender
+    public delegate void Handler<TData>(World world, in TData data);
+    public delegate void SimpleHandler<TData>(World world, TData data);
+    
+    private interface IExecutor
     {
-        void Send(World world);
+        void Execute(World world);
     }
 
-    private class Sender<UCommand> : ISender
+    private class Sender<UCommand> : IExecutor
         where UCommand : TCommand
     {
-        public EntityRef Target;
-        public UCommand Command;
+        private readonly EntityRef _target;
+        private readonly UCommand _command;
 
         public Sender(in EntityRef target, in UCommand command)
         {
-            Target = target;
-            Command = command;
+            _target = target;
+            _command = command;
         }
 
-        public void Send(World world)
-            => world.Send(Target, Command);
+        public void Execute(World world)
+            => world.Send(_target, _command);
+    }
+
+    private class CustomAction : IExecutor
+    {
+        private readonly Action<World> _handler;
+
+        public CustomAction(Action<World> handler)
+        {
+            _handler = handler;
+        }
+
+        public void Execute(World world)
+            => _handler(world);
+    }
+
+    private class CustomAction<TData> : IExecutor
+    {
+        private readonly TData _data;
+        private readonly Handler<TData> _action;
+
+        public CustomAction(in TData data, Handler<TData> action)
+        {
+            _data = data;
+            _action = action;
+        }
+
+        public void Execute(World world)
+            => _action(world, _data);
+    }
+
+    private class SimpleCustomAction<TData> : IExecutor
+    {
+        private readonly TData _data;
+        private readonly SimpleHandler<TData> _action;
+
+        public SimpleCustomAction(in TData data, SimpleHandler<TData> action)
+        {
+            _data = data;
+            _action = action;
+        }
+
+        public void Execute(World world)
+            => _action(world, _data);
     }
 
     public World World { get; }
 
-    private readonly ThreadLocal<List<ISender>> _senders = new(() => new(), true);
+    private readonly ThreadLocal<List<IExecutor>> _executors = new(() => new(), true);
 
     internal WorldCommandBuffer(World world)
     {
@@ -41,15 +87,27 @@ public class WorldCommandBuffer<TCommand>
         where UCommand : TCommand
     {
         command.Execute(World, target);
-        _senders.Value!.Add(new Sender<UCommand>(target, command));
+        _executors.Value!.Add(new Sender<UCommand>(target, command));
     }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Do(Action<World> handler)
+        => _executors.Value!.Add(new CustomAction(handler));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Do<TData>(in TData data, Handler<TData> handler)
+        => _executors.Value!.Add(new CustomAction<TData>(data, handler));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Do<TData>(in TData data, SimpleHandler<TData> handler)
+        => _executors.Value!.Add(new SimpleCustomAction<TData>(data, handler));
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Submit()
     {
-        foreach (var senders in _senders.Values) {
+        foreach (var senders in _executors.Values) {
             foreach (var sender in CollectionsMarshal.AsSpan(senders)) {
-                sender.Send(World);
+                sender.Execute(World);
             }
             senders.Clear();
         }
