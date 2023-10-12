56cc1201886c9a51805ddb479b5bdcc64f7699f9
Phlam Sicusa <sicusa@gilatod.art>
2023-10-12 11:01:05 +0800

    add support for view

---
+++ Sia/Views/ViewBase.cs
@@ -0,0 +1,46 @@
+namespace Sia;
+
+using System.Diagnostics.CodeAnalysis;
+
+public abstract class ViewBase<TTypeUnion> : IAddon
+    where TTypeUnion : ITypeUnion, new()
+{
+    [AllowNull]
+    public World World { get; private set; }
+
+    protected World.EntityQuery? Query { get; private set; }
+
+    public virtual void OnInitialize(World world)
+    {
+        World = world;
+        Query = world.Query<TTypeUnion>();
+        Query.OnEntityHostAdded += OnEntityHostAdded;
+
+        foreach (var host in Query.Hosts) {
+            OnEntityHostAdded(host);
+        }
+    }
+
+    private void OnEntityHostAdded(IReactiveEntityHost host)
+    {
+        host.OnEntityCreated += OnEntityAdded;
+        host.OnEntityReleased += OnEntityRemoved;
+
+        host.IterateAllocated((this, host),
+            static (in (ViewBase<TTypeUnion> View, IReactiveEntityHost Host) data, long pointer) =>
+                data.View.OnEntityAdded(new(pointer, data.Host)));
+    }
+
+    public virtual void OnUninitialize(World world)
+    {
+        foreach (var host in Query!.Hosts) {
+            host.OnEntityCreated -= OnEntityAdded;
+            host.OnEntityReleased -= OnEntityRemoved;
+        }
+        Query.Dispose();
+        Query = null;
+    }
+
+    protected abstract void OnEntityAdded(in EntityRef entity);
+    protected abstract void OnEntityRemoved(in EntityRef entity);
+}
