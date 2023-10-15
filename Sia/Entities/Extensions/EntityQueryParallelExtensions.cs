0e78b9d26df429b53a2f385ec9b4aff27e79e54a
Phlam Sicusa <sicusa@gilatod.art>
2023-10-15 10:37:34 +0800

    optimize entity parallel query

--- Sia/Entities/Extensions/EntityQueryParallelExtensions.cs
+++ Sia/Entities/Extensions/EntityQueryParallelExtensions.cs
@@ -1,110 +1,115 @@
 namespace Sia;
 
+using System.Collections.Concurrent;
 using System.Runtime.CompilerServices;
 using CommunityToolkit.HighPerformance.Buffers;
-using CommunityToolkit.HighPerformance.Helpers;
 
 public static class EntityQueryParallelExtensions
 {
-    #pragma warning disable CS8500
-
-    public unsafe readonly struct ForEachParallelAction : IAction
+    public readonly struct ForEachParallelAction
     {
-        private readonly EntityRef* _memory;
+        private readonly EntityRef[] _array;
         private readonly EntityHandler _handler;
 
-        public ForEachParallelAction(EntityRef* memory, EntityHandler handler)
+        public ForEachParallelAction(EntityRef[] array, EntityHandler handler)
         {
-            _memory = memory;
+            _array = array;
             _handler = handler;
         }
 
-        public void Invoke(int index)
-            => _handler(_memory[index]);
+        public void Invoke(System.Tuple<int, int> range)
+        {
+            for (int i = range.Item1; i != range.Item2; ++i) {
+                _handler(_array[i]);
+            }
+        }
     }
 
-    public unsafe readonly struct ForEachParallelAction<TData> : IAction
+    public readonly struct ForEachParallelAction<TData>
     {
-        private readonly EntityRef* _memory;
+        private readonly EntityRef[] _array;
         private readonly TData _data;
         private readonly EntityHandler<TData> _handler;
 
-        public ForEachParallelAction(EntityRef* memory, in TData data, EntityHandler<TData> handler)
+        public ForEachParallelAction(EntityRef[] array, in TData data, EntityHandler<TData> handler)
         {
-            _memory = memory;
+            _array = array;
             _data = data;
             _handler = handler;
         }
 
-        public void Invoke(int index)
-            => _handler(_data, _memory[index]);
+        public void Invoke(System.Tuple<int, int> range)
+        {
+            for (int i = range.Item1; i != range.Item2; ++i) {
+                _handler(_data, _array[i]);
+            }
+        }
     }
 
     public unsafe struct ForEachParallelData
     {
         public int* Index;
-        public EntityRef* Memory;
+        public EntityRef[] Array;
         
-        public ForEachParallelData(int* index, EntityRef* memory)
+        public ForEachParallelData(int* index, EntityRef[] array)
         {
             Index = index;
-            Memory = memory;
+            Array = array;
         }
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe void ForEachParallel(this IEntityQuery query, EntityHandler handler, int minimumActionsPerThread = 1)
+    private unsafe static void RecordEntities(IEntityQuery query, EntityRef[] array)
+    {
+        int index = 0;
+        var indexPtr = (int*)Unsafe.AsPointer(ref index);
+
+        query.ForEach(new(indexPtr, array), static (in ForEachParallelData data, in EntityRef entity) => {
+            ref int index = ref *data.Index;
+            data.Array[index] = entity;
+            ++index;
+        });
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static unsafe void ForEachParallel(this IEntityQuery query, EntityHandler handler)
     {
         var count = query.Count;
         if (count == 0) { return; }
 
         using var spanOwner = SpanOwner<EntityRef>.Allocate(count);
-        var memory = (EntityRef*)Unsafe.AsPointer(ref spanOwner.Span[0]);
+        var array = spanOwner.DangerousGetArray().Array!;
+        RecordEntities(query, array);
 
-        int index = 0;
-        var indexPtr = (int*)Unsafe.AsPointer(ref index);
-
-        query.ForEach(new(indexPtr, memory), static (in ForEachParallelData span, in EntityRef entity) => {
-            ref int index = ref *span.Index;
-            span.Memory[index] = entity;
-            ++index;
-        });
-
-        ParallelHelper.For(0, count, new ForEachParallelAction(memory, handler), minimumActionsPerThread);
+        var action = new ForEachParallelAction(array, handler);
+        Partitioner.Create(0, count)
+            .AsParallel()
+            .ForAll(action.Invoke);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe void ForEachParallel<TData>(this IEntityQuery query, in TData data, EntityHandler<TData> handler, int minimumActionsPerThread = 1)
+    public static unsafe void ForEachParallel<TData>(this IEntityQuery query, in TData data, EntityHandler<TData> handler)
     {
         var count = query.Count;
         if (count == 0) { return; }
 
         using var spanOwner = SpanOwner<EntityRef>.Allocate(count);
-        var memory = (EntityRef*)Unsafe.AsPointer(ref spanOwner.Span[0]);
+        var array = spanOwner.DangerousGetArray().Array!;
+        RecordEntities(query, array);
 
-        int index = 0;
-        var indexPtr = (int*)Unsafe.AsPointer(ref index);
-
-        query.ForEach(new(indexPtr, memory), static (in ForEachParallelData span, in EntityRef entity) => {
-            ref int index = ref *span.Index;
-            span.Memory[index] = entity;
-            ++index;
-        });
-
-        ParallelHelper.For(0, count, new ForEachParallelAction<TData>(memory, data, handler), minimumActionsPerThread);
+        var action = new ForEachParallelAction<TData>(array, data, handler);
+        Partitioner.Create(0, count)
+            .AsParallel()
+            .ForAll(action.Invoke);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe void ForEachParallel(this IEntityQuery query, SimpleEntityHandler handler, int minimumActionsPerThread = 1)
+    public static unsafe void ForEachParallel(this IEntityQuery query, SimpleEntityHandler handler)
         => ForEachParallel(query, handler,
-            static (in SimpleEntityHandler handler, in EntityRef entity) => handler(entity),
-            minimumActionsPerThread);
+            static (in SimpleEntityHandler handler, in EntityRef entity) => handler(entity));
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static unsafe void ForEachParallel<TData>(this IEntityQuery query, in TData data, SimpleEntityHandler<TData> handler, int minimumActionsPerThread = 1)
+    public static unsafe void ForEachParallel<TData>(this IEntityQuery query, in TData data, SimpleEntityHandler<TData> handler)
         => ForEachParallel(query, (handler, data),
-            static (in (SimpleEntityHandler<TData>, TData) data, in EntityRef entity) => data.Item1(data.Item2, entity),
-            minimumActionsPerThread);
-
-    #pragma warning restore CS8500
+            static (in (SimpleEntityHandler<TData>, TData) data, in EntityRef entity) => data.Item1(data.Item2, entity));
 }
