f95ff13a748cce17aa5c4fbf4a6e83593857c06a
Phlam Sicusa <sicusa@gilatod.art>
2023-10-15 17:08:32 +0800

    remove OnTriggerEvent & OnFilterEvent for system

--- Sia/Systems/Interfaces/ISystem.cs
+++ Sia/Systems/Interfaces/ISystem.cs
@@ -9,12 +9,5 @@ public interface ISystem
 
     void Initialize(World world, Scheduler scheduler);
     void Uninitialize(World world, Scheduler scheduler);
-
     void Execute(World world, Scheduler scheduler, IEntityQuery query);
-
-    bool OnTriggerEvent<TEvent>(World world, Scheduler scheduler, in EntityRef entity, in TEvent e)
-        where TEvent : IEvent;
-
-    bool OnFilterEvent<TEvent>(World world, Scheduler scheduler, in EntityRef entity, in TEvent e)
-        where TEvent : IEvent;
 }
