64124d1bd74f7ac97e0f12518561c1c80642fe16
Phlam Sicusa <sicusa@gilatod.art>
2023-10-19 15:01:48 +0800

    refine example program output

--- Sia.Examples/Program.cs
+++ Sia.Examples/Program.cs
@@ -1,9 +1,17 @@
 using Sia.Examples;
 
+void Invoke(Action action)
+{
+    Console.WriteLine("== " + action.Method.DeclaringType + " ==");
+    action();
+    Console.WriteLine();
+}
+
 Tests.Run();
-Example1_HealthDamage.Run();
-Example2_HealthRecover.Run();
-Example3_MoveRotator.Run();
-Example4_Aggregator.Run();
-Example5_ComponentBundle.Run();
-Example6_Hierarchy.Run();
+Invoke(Example1_HealthDamage.Run);
+Invoke(Example2_HealthRecover.Run);
+Invoke(Example3_MoveRotator.Run);
+Invoke(Example4_Aggregator.Run);
+Invoke(Example5_ComponentBundle.Run);
+Invoke(Example6_Hierarchy.Run);
+Invoke(Example7_Mapper.Run);
