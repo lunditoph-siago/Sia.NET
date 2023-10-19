debcbaaeeea356ae2ff08343465af6eb2487865e
Phlam Sicusa <sicusa@gilatod.art>
2023-10-19 15:01:38 +0800

    add mapper view

---
+++ Sia/Views/Mapper/Mapper.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Sia;
+
+public class Mapper<TId> : ViewBase<TypeUnion<Sid<TId>>>, IReadOnlyDictionary<TId, EntityRef>
+    where TId : IEquatable<TId>
+{
+    public IEnumerable<TId> Keys => _maps.Keys;
+    public IEnumerable<EntityRef> Values => _maps.Values;
+
+    public int Count => _maps.Count;
+
+    public EntityRef this[TId key] => _maps[key];
+
+    private readonly Dictionary<TId, EntityRef> _maps = new();
+
+    public override void OnInitialize(World world)
+    {
+        world.Dispatcher.Listen<Sid<TId>.SetValue>(OnEntityIdChanged);
+        base.OnInitialize(world);
+    }
+
+    public override void OnUninitialize(World world)
+    {
+        base.OnUninitialize(world);
+        world.Dispatcher.Unlisten<Sid<TId>.SetValue>(OnEntityIdChanged);
+    }
+
+    private bool OnEntityIdChanged(in EntityRef entity, in Sid<TId>.SetValue e)
+    {
+        ref var id = ref entity.Get<Sid<TId>>();
+        RemoveMap(entity, id.Previous);
+        AddMap(entity, id.Value);
+        return false;
+    }
+
+    protected override void OnEntityAdded(in EntityRef entity)
+    {
+        var id = entity.Get<Sid<TId>>().Value;
+        AddMap(entity, id);
+    }
+
+    protected override void OnEntityRemoved(in EntityRef entity)
+    {
+        var id = entity.Get<Sid<TId>>().Value;
+        RemoveMap(entity, id);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void AddMap(in EntityRef entity, in TId id)
+    {
+        _maps[id] = entity;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void RemoveMap(in EntityRef entity, in TId id)
+    {
+        if (!_maps.Remove(id, out var removedEntity)) {
+            return;
+        }
+        if (removedEntity != entity) {
+            _maps[id] = removedEntity;
+        }
+    }
+
+    public bool ContainsKey(TId key)
+        => _maps.ContainsKey(key);
+
+    public bool TryGetValue(TId key, [MaybeNullWhen(false)] out EntityRef value)
+        => _maps.TryGetValue(key, out value);
+
+    public IEnumerator<KeyValuePair<TId, EntityRef>> GetEnumerator()
+        => _maps.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => _maps.GetEnumerator();
+}
