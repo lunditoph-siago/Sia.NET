f51d1394f36b32d2cc7ff11c677f65e43850eb37
Phlam Sicusa <sicusa@gilatod.art>
2023-10-29 16:16:29 +0800

    add template example

---
+++ Sia.Examples/Example9_Templates.cs
@@ -0,0 +1,25 @@
+namespace Sia.Examples;
+
+using System.Collections.Immutable;
+using System.Numerics;
+
+public static partial class Example9_Templates
+{
+    public abstract record Test
+    {
+        public Guid? Id { get; set; }
+    }
+
+    [SiaTemplate("TestObject")]
+    public record TestTemplate<T> : Test
+    {
+        public T? Value { get; set; }
+
+        [SiaProperty(Item = "TestItem")]
+        public ImmutableDictionary<Vector2, string> Test = ImmutableDictionary<Vector2, string>.Empty;
+
+        [SiaProperty(Item = "TestItem2")]
+        public ImmutableArray<Vector2> Test2 = ImmutableArray<Vector2>.Empty;
+    }
+
+}
