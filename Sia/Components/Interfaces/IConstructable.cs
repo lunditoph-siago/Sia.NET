0a4ffdcbde5441d291ce0060f697794ab712e1bb
Phlam Sicusa <sicusa@gilatod.art>
2023-11-18 13:07:12 +0800

    small refinements

--- Sia/Components/Interfaces/IConstructable.cs
+++ Sia/Components/Interfaces/IConstructable.cs
@@ -2,5 +2,5 @@ namespace Sia;
 
 public interface IConstructable<TComponent, TTemplate>
 {
-    abstract static void Construct(TTemplate template, out TComponent result);
+    abstract static void Construct(TTemplate template, out TComponent component);
 }
