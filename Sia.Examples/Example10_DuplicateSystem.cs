50b31def40fe119dd9755800d265330105dccf04
Phlam Sicusa <sicusa@gilatod.art>
2023-12-01 01:12:46 +0800

    add examples for registering duplicate systems

---
+++ Sia.Examples/Example10_DuplicateSystem.cs
@@ -0,0 +1,34 @@
+namespace Sia.Examples;
+
+public static class Example10_DuplicateSystem
+{
+    public class PrintSystem : SystemBase
+    {
+        private readonly string _text;
+
+        public PrintSystem(string text)
+        {
+            _text = text;
+            Matcher = Matchers.Any;
+        }
+
+        public override void Execute(World world, Scheduler scheduler, IEntityQuery query)
+        {
+            Console.WriteLine(_text);
+        }
+    }
+
+    public static void Run()
+    {
+        var world = new World();
+        var scheduler = new Scheduler();
+
+        SystemChain.Empty
+            .Add<PrintSystem>(() => new("Hello!"))
+            .Add<PrintSystem>(() => new("World!"))
+            .RegisterTo(world, scheduler);
+        
+        scheduler.Tick();
+        scheduler.Tick();
+    }
+}
