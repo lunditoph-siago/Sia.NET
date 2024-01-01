676ca83f7ef23ff8e1ef33c34ef6a9a8f5a00274
Phlam Sicusa <sicusa@gilatod.art>
2024-01-01 20:10:29 +0800

    refine entity valid check

--- Sia/Entities/EntityRef.cs
+++ Sia/Entities/EntityRef.cs
@@ -3,7 +3,7 @@ namespace Sia;
 public readonly record struct EntityRef(nint Pointer, int Version, IEntityHost Host) : IDisposable
 {
     public object Boxed => Host.Box(Pointer, Version);
-    public bool Valid => Host.IsValid(Pointer, Version);
+    public bool Valid => Host != null && Host.IsValid(Pointer, Version);
     
     public bool Contains<TComponent>()
         => Host.Contains<TComponent>(Pointer, Version);
