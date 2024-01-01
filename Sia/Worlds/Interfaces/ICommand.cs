0e08076df72ba9747495bf550524790ae116717a
Phlam Sicusa <sicusa@gilatod.art>
2024-01-01 15:25:32 +0800

    make generic version of ICommand sub-interface of ICommand

--- Sia/Worlds/Interfaces/ICommand.cs
+++ Sia/Worlds/Interfaces/ICommand.cs
@@ -5,7 +5,7 @@ public interface ICommand : IEvent
     void Execute(World world, in EntityRef target);
 }
 
-public interface ICommand<T> : IEvent
+public interface ICommand<T> : ICommand
 {
     void Execute(World world, in EntityRef target, ref T component);
 }
