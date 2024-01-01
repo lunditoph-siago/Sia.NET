ba1db23616f6821310128c447989fb9b83f4b0de
Phlam Sicusa <sicusa@gilatod.art>
2024-01-01 16:43:19 +0800

    make IParallelCommand sub-interface of generic ICommand

--- Sia/Worlds/Interfaces/IParallelCommand.cs
+++ Sia/Worlds/Interfaces/IParallelCommand.cs
@@ -5,7 +5,7 @@ public interface IParallelCommand : ICommand
     void ExecuteOnParallel(in EntityRef target);
 }
 
-public interface IParallelCommand<TComponent> : IParallelCommand
+public interface IParallelCommand<TComponent> : IParallelCommand, ICommand<TComponent>
 {
     void ExecuteOnParallel(ref TComponent component);
 }
