1405809a0e3e10e948d83d88ef5bcee7748c681f
Phlam Sicusa <sicusa@gilatod.art>
2024-01-02 11:15:19 +0800

    adapt new C# grammar of constructor

--- Sia/Systems/SystemBase.cs
+++ Sia/Systems/SystemBase.cs
@@ -7,6 +7,17 @@ public abstract class SystemBase : ISystem
     public IEventUnion? Trigger { get; init; }
     public IEventUnion? Filter { get; init; }
 
+    public SystemBase() {}
+    public SystemBase(
+        SystemChain? children = null, IEntityMatcher? matcher = null,
+        IEventUnion? trigger = null, IEventUnion? filter = null)
+    {
+        Children = children;
+        Matcher = matcher;
+        Trigger = trigger;
+        Filter = filter;
+    }
+
     public virtual void Initialize(World world, Scheduler scheduler) {}
     public virtual void Uninitialize(World world, Scheduler scheduler) {}
     public virtual void Execute(World world, Scheduler scheduler, IEntityQuery query) {}
