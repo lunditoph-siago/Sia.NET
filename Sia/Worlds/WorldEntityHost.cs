476761099f5cd18f1a7ce55feec8b5d52ce7abec
Phlam Sicusa <sicusa@gilatod.art>
2024-03-21 10:56:52 +0800

    fix WrappedWorldEntityHost

--- Sia/Worlds/WorldEntityHost.cs
+++ Sia/Worlds/WorldEntityHost.cs
@@ -2,46 +2,14 @@ namespace Sia;
 
 using System.Runtime.CompilerServices;
 
+using static WorldHostUtils;
+
 public class WorldEntityHost<TEntity, TStorage>(
     World world, TStorage storage, IEntityHostProvider siblingHostProvider)
     : StorageEntityHost<TEntity, TStorage>(storage), IReactiveEntityHost
     where TEntity : IHList
     where TStorage : IStorage<HList<Identity, TEntity>>
 {
-    private struct EntityHeadAddEventSender(EntityRef entity, WorldDispatcher dispatcher) : IGenericHandler
-    {
-        public readonly void Handle<T>(in T value)
-        {
-            dispatcher.Send(entity, WorldEvents.Add<T>.Instance);
-        }
-    }
-
-    private struct EntityTailAddEventSender(EntityRef entity, WorldDispatcher dispatcher) : IGenericHandler<IHList>
-    {
-        public readonly void Handle<T>(in T value) where T : IHList
-        {
-            value.HandleHead(new EntityHeadAddEventSender(entity, dispatcher));
-            value.HandleTail(new EntityTailAddEventSender(entity, dispatcher));
-        }
-    }
-
-    private struct EntityHeadRemoveEventSender(EntityRef entity, WorldDispatcher dispatcher) : IGenericHandler
-    {
-        public readonly void Handle<T>(in T value)
-        {
-            dispatcher.Send(entity, WorldEvents.Remove<T>.Instance);
-        }
-    }
-
-    private struct EntityTailRemoveEventSender(EntityRef entity, WorldDispatcher dispatcher) : IGenericHandler<IHList>
-    {
-        public readonly void Handle<T>(in T value) where T : IHList
-        {
-            value.HandleHead(new EntityHeadRemoveEventSender(entity, dispatcher));
-            value.HandleTail(new EntityTailRemoveEventSender(entity, dispatcher));
-        }
-    }
-
     public event EntityHandler? OnEntityCreated;
     public event EntityHandler? OnEntityReleased;
 
