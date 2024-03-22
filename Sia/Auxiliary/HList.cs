76128baf1e7861e163f54de0f9a40258325c1885
Valtameri Sieluna <seele.peng@gmail.com>
2024-03-22 08:21:59 +0200

    perf: inline low complexity code (#12)

--- Sia/Auxiliary/HList.cs
+++ Sia/Auxiliary/HList.cs
@@ -155,45 +155,56 @@ public struct HList<THead, TTail>(in THead head, in TTail tail) : IHList
 
 public static class HList
 {
-    public static HList<THead, EmptyHList> Create<THead>(in THead head)
-        => new(head, EmptyHList.Default);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static HList<THead, EmptyHList> Create<THead>(in THead head) =>
+        new HList<THead, EmptyHList>(head, EmptyHList.Default);
     
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static HList<THead, TTail> Cons<THead, TTail>(in THead head, in TTail tail)
         where TTail : IHList
         => new(head, tail);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static HList<T1, HList<T2, EmptyHList>>
         Create<T1, T2>(in T1 item1, in T2 item2)
         => Cons(item1, Create(item2));
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static HList<T1, HList<T2, HList<T3, EmptyHList>>>
         Create<T1, T2, T3>(in T1 item1, in T2 item2, in T3 item3)
         => Cons(item1, Cons(item2, Create(item3)));
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static HList<T1, HList<T2, HList<T3, HList<T4, EmptyHList>>>>
         Create<T1, T2, T3, T4>(in T1 item1, in T2 item2, in T3 item3, in T4 item4)
         => Cons(item1, Cons(item2, Cons(item3, Create(item4))));
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static HList<T1, HList<T2, HList<T3, HList<T4, HList<T5, EmptyHList>>>>>
         Create<T1, T2, T3, T4, T5>(in T1 item1, in T2 item2, in T3 item3, in T4 item4, in T5 item5)
         => Cons(item1, Cons(item2, Cons(item3, Cons(item4, Create(item5)))));
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static HList<T1, HList<T2, HList<T3, HList<T4, HList<T5, HList<T6, EmptyHList>>>>>>
         Create<T1, T2, T3, T4, T5, T6>(in T1 item1, in T2 item2, in T3 item3, in T4 item4, in T5 item5, in T6 item6)
         => Cons(item1, Cons(item2, Cons(item3, Cons(item4, Cons(item5, Create(item6))))));
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static HList<T1, HList<T2, HList<T3, HList<T4, HList<T5, HList<T6, HList<T7, EmptyHList>>>>>>>
         Create<T1, T2, T3, T4, T5, T6, T7>(in T1 item1, in T2 item2, in T3 item3, in T4 item4, in T5 item5, in T6 item6, in T7 item7)
         => Cons(item1, Cons(item2, Cons(item3, Cons(item4, Cons(item5, Cons(item6, Create(item7)))))));
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static HList<T1, HList<T2, HList<T3, HList<T4, HList<T5, HList<T6, HList<T7, HList<T8, EmptyHList>>>>>>>>
         Create<T1, T2, T3, T4, T5, T6, T7, T8>(in T1 item1, in T2 item2, in T3 item3, in T4 item4, in T5 item5, in T6 item6, in T7 item7, in T8 item8)
         => Cons(item1, Cons(item2, Cons(item3, Cons(item4, Cons(item5, Cons(item6, Cons(item7, Create(item8))))))));
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static HList<T1, HList<T2, HList<T3, HList<T4, HList<T5, HList<T6, HList<T7, HList<T8, HList<T9, EmptyHList>>>>>>>>>
         Create<T1, T2, T3, T4, T5, T6, T7, T8, T9>(in T1 item1, in T2 item2, in T3 item3, in T4 item4, in T5 item5, in T6 item6, in T7 item7, in T8 item8, in T9 item9)
         => Cons(item1, Cons(item2, Cons(item3, Cons(item4, Cons(item5, Cons(item6, Cons(item7, Cons(item8, Create(item9)))))))));
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static HList<T1, HList<T2, HList<T3, HList<T4, HList<T5, HList<T6, HList<T7, HList<T8, HList<T9, HList<T10, EmptyHList>>>>>>>>>>
         Create<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(in T1 item1, in T2 item2, in T3 item3, in T4 item4, in T5 item5, in T6 item6, in T7 item7, in T8 item8, in T9 item9, in T10 item10)
         => Cons(item1, Cons(item2, Cons(item3, Cons(item4, Cons(item5, Cons(item6, Cons(item7, Cons(item8, Cons(item9, Create(item10))))))))));
