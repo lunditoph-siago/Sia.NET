bdcb53aa4f7538df26ed0a9aa642122f53f07d6f
Phlam Sicusa <sicusa@gilatod.art>
2024-03-28 18:09:08 +0800

    remove nullable modifier for World.Current

--- Sia/Worlds/Extensions/EntityWorldContextExtensions.cs
+++ Sia/Worlds/Extensions/EntityWorldContextExtensions.cs
@@ -7,16 +7,16 @@ public static class EntityWorldContextExtensions
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Send<TEvent>(this EntityRef entity, in TEvent e)
         where TEvent : IEvent
-        => World.Current!.Send(entity, e);
+        => World.Current.Send(entity, e);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Modify<TCommand>(this EntityRef entity, in TCommand command)
         where TCommand : ICommand
-        => World.Current!.Modify(entity, command);
+        => World.Current.Modify(entity, command);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Modify<TComponent, TCommand>(
         this EntityRef entity, ref TComponent component, in TCommand command)
         where TCommand : ICommand<TComponent>
-        => World.Current!.Modify(entity, ref component, command);
+        => World.Current.Modify(entity, ref component, command);
 }
