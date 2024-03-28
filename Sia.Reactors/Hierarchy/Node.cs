bf1e996cbd5a3b7a404d00c1b19352c940dfa095
Phlam Sicusa <sicusa@gilatod.art>
2024-03-29 02:57:11 +0800

    add support for enabling/disabling nodes

--- Sia.Reactors/Hierarchy/Node.cs
+++ Sia.Reactors/Hierarchy/Node.cs
@@ -4,8 +4,14 @@ using System.Collections.Immutable;
 
 public partial record struct Node<TTag>
 {
+    public readonly record struct OnIsEnabledChanged(bool Enabled) : IEvent;
     public readonly record struct ChildAdded(EntityRef Entity) : IEvent;
     public readonly record struct ChildRemoved(EntityRef Entity) : IEvent;
+
+    public readonly bool IsEnabled => _enabled;
+
+    [Sia]
+    public bool IsSelfEnabled { get; set; } = true;
 
     [Sia]
     public Identity? Parent {
@@ -18,6 +24,8 @@ public partial record struct Node<TTag>
 
     public readonly IReadOnlySet<Identity> Children =>
         _children ?? (IReadOnlySet<Identity>)ImmutableHashSet<Identity>.Empty;
+
+    internal bool _enabled = true;
 
     internal Identity? _parent;
     internal Identity? _prevParent;
