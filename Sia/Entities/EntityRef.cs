400a41d0eb63d05a20aef027b6370d4b8e31a798
Phlam Sicusa <sicusa@gilatod.art>
2024-03-29 22:53:06 +0800

    fix EntityRef.GetHList

--- Sia/Entities/EntityRef.cs
+++ Sia/Entities/EntityRef.cs
@@ -88,9 +88,9 @@ public readonly record struct EntityRef(in StorageSlot Slot, IEntityHost Host) :
     public EntityRef Remove<TComponent>()
         => Host.Remove<TComponent>(Slot);
 
-    public void GetHList<THandler>(in StorageSlot slot, in THandler handler)
+    public void GetHList<THandler>(in THandler handler)
         where THandler : IRefGenericHandler<IHList>
-        => Host.GetHList(slot, handler);
+        => Host.GetHList(Slot, handler);
 
     public Span<byte> AsSpan()
         => Host.GetSpan(Slot);
