2bfe1a88bbfda12849a4dc0cdedff27de0680216
GPU-Fried-Eggs <seele.peng@gmail.com>
2024-03-31 02:09:11 +0200

    test: add matcher and dispatcher tests

---
+++ Sia.Tests/Entities/MatcherTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Sia.Tests.Entities;
+
+public class MatcherTests
+{
+    public static List<object[]> MatcherTestData =>
+    [
+        [Matchers.Of<int>(), new TypeUnion<int>().ToMatcher(), true],
+        [Matchers.Of<int>(), new TypeUnion<long>().ToMatcher(), false],
+        [Matchers.Of<int, float>().With(new TypeUnion<long>()), new TypeUnion<int, float, long>().ToMatcher(), true]
+    ];
+
+    [Theory]
+    [MemberData(nameof(MatcherTestData))]
+    public void Matcher_Equality_Test(IEntityMatcher left, IEntityMatcher right, bool result)
+    {
+        Assert.Equal(result, left.Equals(right));
+    }
+}
---
+++ Sia.Tests/Events/DispatcherTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Sia.Tests.Events;
+
+public class DispatcherTests
+{
+    public readonly record struct AssertCommand(int Expected) : ICommand
+    {
+        public void Execute(World world, in EntityRef target) { }
+    }
+
+    [Fact]
+    public void Dispatcher_Test()
+    {
+        var dispatcher = new Dispatcher<int, IEvent>();
+        dispatcher.Listen((in int target, in AssertCommand e) => {
+            Assert.Equal(e.Expected, target);
+            return false;
+        });
+
+        dispatcher.Send(1, new AssertCommand(1));
+        dispatcher.Send(2, new AssertCommand(2));
+    }
+}
