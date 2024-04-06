e6b0455332178a1c75a493f160f168fb58162078
Valtameri Sieluna <seele.peng@gmail.com>
2024-04-07 02:59:38 +0300

    test: add few extra tests (#20)
    * Add more coverage (#15)
    
    * fix: replace the test data and assert
    
    * test: Add missing bundle and template
    
    * fix: fix few failure test
    
    * test: add scheduler and storage test
    
    * test: add matcher and dispatcher tests
    
    * test: add blocking queue and sparseSet test

---
+++ Sia.Tests/Storages/BufferStorageTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Sia.Tests.Storages;
+
+public class BufferStorageTests
+{
+    public static List<object[]> BufferStorageTestData =>
+    [
+        [new ArrayBufferStorage<int>(5120)],
+        [new SparseBufferStorage<int>(5120)],
+        [new HashBufferStorage<int>()]
+    ];
+
+    [Theory]
+    [MemberData(nameof(BufferStorageTestData))]
+    public void BufferStorage_Test<TStorage>(TStorage storage)
+        where TStorage : class, IStorage<int>
+    {
+        var pointers = new List<Pointer<int>>();
+            
+        for (var c = 0; c < 10; c++) {
+            if (Random.Shared.NextSingle() < 0.5) {
+                var count = Random.Shared.Next(1, 30);
+                for (var i = 0; i < count; ++i) {
+                    pointers.Add(storage.Allocate());
+                }
+            }
+            else {
+                while (pointers.Count > 0) {
+                    var index = Random.Shared.Next(0, pointers.Count);
+                    pointers[index].Dispose();
+                    pointers.RemoveAt(index);
+                }
+            }
+        }
+
+        while (pointers.Count > 0) {
+            var index = Random.Shared.Next(0, pointers.Count);
+            pointers[index].Dispose();
+            pointers.RemoveAt(index);
+        }
+    }
+}
