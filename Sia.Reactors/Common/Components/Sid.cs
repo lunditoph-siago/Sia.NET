d7449a44619e381d653d5fe672ed53444f54465c
Phlam Sicusa <sicusa@gilatod.art>
2024-04-12 15:56:07 +0800

    refactor Sid

--- Sia.Reactors/Common/Components/Sid.cs
+++ Sia.Reactors/Common/Components/Sid.cs
@@ -1,26 +1,21 @@
 namespace Sia.Reactors;
 
 // SID: Sia Identifier
-public record struct Sid<TId>(TId Value)
+public partial struct Sid<TId>(TId value)
     where TId : notnull
 {
-    public TId Previous { get; private set; }
-
-    public readonly record struct SetValue(TId Value) : IParallelCommand, IReconstructableCommand<SetValue>
-    {
-        public static SetValue ReconstructFromCurrentState(in EntityRef entity)
-            => new(entity.Get<Sid<TId>>().Value);
-
-        public void Execute(World world, in EntityRef target)
-            => ExecuteOnParallel(target);
-
-        public void ExecuteOnParallel(in EntityRef target)
-        {
-            ref var id = ref target.Get<Sid<TId>>();
-            id.Previous = id.Value;
-            id.Value = Value;
+    [Sia]
+    public TId Value {
+        readonly get => _value;
+        set {
+            Previous = _value;
+            _value = value;
         }
     }
+
+    public TId Previous { get; private set; }
+
+    private TId _value = value;
 }
 
 public static class Sid
