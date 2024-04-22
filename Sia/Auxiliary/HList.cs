283384b6a6a8c2dd4334c8e2981e52ba51e4ec15
Phlam Sicusa <sicusa@gilatod.art>
2024-04-23 01:10:20 +0800

    move generic handlers to separate file

--- Sia/Auxiliary/HList.cs
+++ Sia/Auxiliary/HList.cs
@@ -7,26 +7,6 @@ public struct TypeProxy<T>
     public static TypeProxy<T> Default => default;
 }
 
-public interface IGenericHandler
-{
-    public void Handle<T>(in T value);
-}
-
-public interface IGenericHandler<TBase>
-{
-    public void Handle<T>(in T value) where T : TBase;
-}
-
-public interface IRefGenericHandler
-{
-    public void Handle<T>(ref T value);
-}
-
-public interface IRefGenericHandler<TBase>
-{
-    public void Handle<T>(ref T value) where T : TBase;
-}
-
 public interface IHList
 {
     public void HandleHead<THandler>(in THandler handler)
