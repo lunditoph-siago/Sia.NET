e30a8710b31684ddbe50ddca6d31f110a092b38e
Phlam Sicusa <sicusa@gilatod.art>
2024-04-23 01:10:20 +0800

    move generic handlers to separate file

---
+++ Sia/Auxiliary/GenericHandlers.cs
@@ -0,0 +1,31 @@
+namespace Sia;
+
+public interface IGenericHandler
+{
+    public void Handle<T>(in T value);
+}
+
+public interface IGenericHandler<TBase>
+{
+    public void Handle<T>(in T value) where T : TBase;
+}
+
+public interface IGenericTypeHandler
+{
+    public void Handle<T>();
+}
+
+public interface IGenericTypeHandler<TBase>
+{
+    public void Handle<T>() where T : TBase;
+}
+
+public interface IRefGenericHandler
+{
+    public void Handle<T>(ref T value);
+}
+
+public interface IRefGenericHandler<TBase>
+{
+    public void Handle<T>(ref T value) where T : TBase;
+}
