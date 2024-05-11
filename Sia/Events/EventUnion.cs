27c92bbdadd254062235ab733cd86cd422f8de92
Phlam Sicusa <sicusa@gilatod.art>
2024-05-11 20:16:54 +0800

    refactor EventUnion

--- Sia/Events/EventUnion.cs
+++ Sia/Events/EventUnion.cs
@@ -1,56 +1,120 @@
 namespace Sia;
 
-public class EventUnion<T1>
-    : TypeUnion<T1>, IEventUnion
-    where T1 : IEvent, new();
-
-public class EventUnion<T1, T2>
-    : TypeUnion<T1, T2>, IEventUnion
+public class EventUnion<T1> : IEventUnion
     where T1 : IEvent, new()
-    where T2 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1>();
 
-public class EventUnion<T1, T2, T3>
-    : TypeUnion<T1, T2, T3>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+    }
+}
+
+public class EventUnion<T1, T2> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
-    where T3 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2>();
 
-public class EventUnion<T1, T2, T3, T4>
-    : TypeUnion<T1, T2, T3, T4>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+    }
+}
+
+public class EventUnion<T1, T2, T3> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
     where T3 : IEvent, new()
-    where T4 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3>();
 
-public class EventUnion<T1, T2, T3, T4, T5>
-    : TypeUnion<T1, T2, T3, T4, T5>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+    }
+}
+
+public class EventUnion<T1, T2, T3, T4> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
     where T3 : IEvent, new()
     where T4 : IEvent, new()
-    where T5 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3, T4>();
 
-public class EventUnion<T1, T2, T3, T4, T5, T6>
-    : TypeUnion<T1, T2, T3, T4, T5, T6>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+        handler.Handle<T4>();
+    }
+}
+
+public class EventUnion<T1, T2, T3, T4, T5> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
     where T3 : IEvent, new()
     where T4 : IEvent, new()
     where T5 : IEvent, new()
-    where T6 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3, T4, T5>();
 
-public class EventUnion<T1, T2, T3, T4, T5, T6, T7>
-    : TypeUnion<T1, T2, T3, T4, T5, T6, T7>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+        handler.Handle<T4>();
+        handler.Handle<T5>();
+    }
+}
+
+public class EventUnion<T1, T2, T3, T4, T5, T6> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
     where T3 : IEvent, new()
     where T4 : IEvent, new()
     where T5 : IEvent, new()
     where T6 : IEvent, new()
-    where T7 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3, T4, T5, T6>();
 
-public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8>
-    : TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+        handler.Handle<T4>();
+        handler.Handle<T5>();
+        handler.Handle<T6>();
+    }
+}
+
+public class EventUnion<T1, T2, T3, T4, T5, T6, T7> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
     where T3 : IEvent, new()
@@ -58,10 +122,25 @@ public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8>
     where T5 : IEvent, new()
     where T6 : IEvent, new()
     where T7 : IEvent, new()
-    where T8 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3, T4, T5, T6, T7>();
 
-public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9>
-    : TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+        handler.Handle<T4>();
+        handler.Handle<T5>();
+        handler.Handle<T6>();
+        handler.Handle<T7>();
+    }
+}
+
+public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
     where T3 : IEvent, new()
@@ -70,10 +149,26 @@ public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9>
     where T6 : IEvent, new()
     where T7 : IEvent, new()
     where T8 : IEvent, new()
-    where T9 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8>();
 
-public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>
-    : TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+        handler.Handle<T4>();
+        handler.Handle<T5>();
+        handler.Handle<T6>();
+        handler.Handle<T7>();
+        handler.Handle<T8>();
+    }
+}
+
+public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
     where T3 : IEvent, new()
@@ -83,10 +178,27 @@ public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>
     where T7 : IEvent, new()
     where T8 : IEvent, new()
     where T9 : IEvent, new()
-    where T10 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9>();
 
-public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>
-    : TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+        handler.Handle<T4>();
+        handler.Handle<T5>();
+        handler.Handle<T6>();
+        handler.Handle<T7>();
+        handler.Handle<T8>();
+        handler.Handle<T9>();
+    }
+}
+
+public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
     where T3 : IEvent, new()
@@ -97,10 +209,28 @@ public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>
     where T8 : IEvent, new()
     where T9 : IEvent, new()
     where T10 : IEvent, new()
-    where T11 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>();
 
-public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>
-    : TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+        handler.Handle<T4>();
+        handler.Handle<T5>();
+        handler.Handle<T6>();
+        handler.Handle<T7>();
+        handler.Handle<T8>();
+        handler.Handle<T9>();
+        handler.Handle<T10>();
+    }
+}
+
+public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
     where T3 : IEvent, new()
@@ -112,10 +242,29 @@ public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>
     where T9 : IEvent, new()
     where T10 : IEvent, new()
     where T11 : IEvent, new()
-    where T12 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>();
 
-public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>
-    : TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+        handler.Handle<T4>();
+        handler.Handle<T5>();
+        handler.Handle<T6>();
+        handler.Handle<T7>();
+        handler.Handle<T8>();
+        handler.Handle<T9>();
+        handler.Handle<T10>();
+        handler.Handle<T11>();
+    }
+}
+
+public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
     where T3 : IEvent, new()
@@ -128,10 +277,30 @@ public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>
     where T10 : IEvent, new()
     where T11 : IEvent, new()
     where T12 : IEvent, new()
-    where T13 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>();
 
-public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>
-    : TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+        handler.Handle<T4>();
+        handler.Handle<T5>();
+        handler.Handle<T6>();
+        handler.Handle<T7>();
+        handler.Handle<T8>();
+        handler.Handle<T9>();
+        handler.Handle<T10>();
+        handler.Handle<T11>();
+        handler.Handle<T12>();
+    }
+}
+
+public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
     where T3 : IEvent, new()
@@ -145,10 +314,31 @@ public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, 
     where T11 : IEvent, new()
     where T12 : IEvent, new()
     where T13 : IEvent, new()
-    where T14 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>();
 
-public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>
-    : TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+        handler.Handle<T4>();
+        handler.Handle<T5>();
+        handler.Handle<T6>();
+        handler.Handle<T7>();
+        handler.Handle<T8>();
+        handler.Handle<T9>();
+        handler.Handle<T10>();
+        handler.Handle<T11>();
+        handler.Handle<T12>();
+        handler.Handle<T13>();
+    }
+}
+
+public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
     where T3 : IEvent, new()
@@ -163,10 +353,32 @@ public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, 
     where T12 : IEvent, new()
     where T13 : IEvent, new()
     where T14 : IEvent, new()
-    where T15 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>();
 
-public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>
-    : TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>, IEventUnion
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+        handler.Handle<T4>();
+        handler.Handle<T5>();
+        handler.Handle<T6>();
+        handler.Handle<T7>();
+        handler.Handle<T8>();
+        handler.Handle<T9>();
+        handler.Handle<T10>();
+        handler.Handle<T11>();
+        handler.Handle<T12>();
+        handler.Handle<T13>();
+        handler.Handle<T14>();
+    }
+}
+
+public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> : IEventUnion
     where T1 : IEvent, new()
     where T2 : IEvent, new()
     where T3 : IEvent, new()
@@ -182,7 +394,75 @@ public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, 
     where T13 : IEvent, new()
     where T14 : IEvent, new()
     where T15 : IEvent, new()
-    where T16 : IEvent, new();
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>();
+
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+        handler.Handle<T4>();
+        handler.Handle<T5>();
+        handler.Handle<T6>();
+        handler.Handle<T7>();
+        handler.Handle<T8>();
+        handler.Handle<T9>();
+        handler.Handle<T10>();
+        handler.Handle<T11>();
+        handler.Handle<T12>();
+        handler.Handle<T13>();
+        handler.Handle<T14>();
+        handler.Handle<T15>();
+    }
+}
+
+public class EventUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> : IEventUnion
+    where T1 : IEvent, new()
+    where T2 : IEvent, new()
+    where T3 : IEvent, new()
+    where T4 : IEvent, new()
+    where T5 : IEvent, new()
+    where T6 : IEvent, new()
+    where T7 : IEvent, new()
+    where T8 : IEvent, new()
+    where T9 : IEvent, new()
+    where T10 : IEvent, new()
+    where T11 : IEvent, new()
+    where T12 : IEvent, new()
+    where T13 : IEvent, new()
+    where T14 : IEvent, new()
+    where T15 : IEvent, new()
+    where T16 : IEvent, new()
+{
+    public static ITypeUnion StaticEventTypes { get; }
+        = new TypeUnion<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>();
+
+    public ITypeUnion EventTypes => StaticEventTypes;
+
+    public static void HandleEventTypes(IGenericTypeHandler<IEvent> handler)
+    {
+        handler.Handle<T1>();
+        handler.Handle<T2>();
+        handler.Handle<T3>();
+        handler.Handle<T4>();
+        handler.Handle<T5>();
+        handler.Handle<T6>();
+        handler.Handle<T7>();
+        handler.Handle<T8>();
+        handler.Handle<T9>();
+        handler.Handle<T10>();
+        handler.Handle<T11>();
+        handler.Handle<T12>();
+        handler.Handle<T13>();
+        handler.Handle<T14>();
+        handler.Handle<T15>();
+        handler.Handle<T16>();
+    }
+}
 
 public static class EventUnion
 {
