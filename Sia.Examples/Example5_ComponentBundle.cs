139376ad99be8949c5e38a47c077227d9e2bc227
Phlam Sicusa <sicusa@gilatod.art>
2024-05-24 01:23:27 +0800

    refine bundle example

--- Sia.Examples/Example5_ComponentBundle.cs
+++ Sia.Examples/Example5_ComponentBundle.cs
@@ -29,65 +29,34 @@ namespace Sia_Examples
         public partial record struct GameObject(Sid<ObjectId> Id, Name Name);
 
         public record struct HP([Sia] int Value);
-        
-        public static class TestObject
-        {
-            public static EntityRef Create(World world)
-            {
-                var transform = new Transform {
-                    Position = new Position {
-                        Value = Vector3.Zero
-                    },
-                    Rotation = new Rotation {
-                        Value = Quaternion.Identity
-                    },
-                    Scale = new Scale {
-                        Value = Vector3.One
-                    }
-                };
-                var gameObject = new GameObject {
-                    Id = new Sid<ObjectId>(0),
-                    Name = "Test"
-                };
-                return world.CreateInArrayHost(HList.Create(new HP(100)))
-                    .AddBundle(transform)
-                    .AddBundle(gameObject);
-            }
 
-            public static EntityRef CreateWithDynBundle(World world)
-            {
-                var transform = new Transform {
-                    Position = new Position {
-                        Value = Vector3.Zero
-                    },
-                    Rotation = new Rotation {
-                        Value = Quaternion.Identity
-                    },
-                    Scale = new Scale {
-                        Value = Vector3.One
-                    }
-                };
-                var gameObject = new GameObject {
-                    Id = new Sid<ObjectId>(0),
-                    Name = "Test"
-                };
-
-                return world.CreateInArrayHost()
-                    .AddBundle(
-                        new DynBundle()
-                            .Add(new HP(100))
-                            .AddBundle(transform)
-                            .AddBundle(gameObject)
-                            .Remove<Scale>());
-            }
-        }
+        public record TestObjectBundle() : DynBundle(
+            new DynBundle()
+                .Add(new HP(100)),
+            new Transform {
+                Position = new Position {
+                    Value = Vector3.Zero
+                },
+                Rotation = new Rotation {
+                    Value = Quaternion.Identity
+                },
+                Scale = new Scale {
+                    Value = Vector3.One
+                }
+            },
+            new GameObject {
+                Id = new Sid<ObjectId>(0),
+                Name = "Test"
+            });
     }
 
     public static partial class Example5_ComponentBundle
     {
         public static void Run(World world)
         {
-            var entity = ComponentBundle.TestObject.Create(world);
+            var entity = world.CreateInArrayHost()
+                .AddBundle(new ComponentBundle.TestObjectBundle());
+
             Console.WriteLine("Entity 1:");
             Console.WriteLine(entity.Get<ComponentBundle.Name>().Value);
             Console.WriteLine(entity.Get<ComponentBundle.HP>().Value);
@@ -97,7 +66,10 @@ namespace Sia_Examples
 
             Console.WriteLine();
 
-            var entity2 = ComponentBundle.TestObject.CreateWithDynBundle(world);
+            var entity2 = world.CreateInArrayHost()
+                .AddBundle(new ComponentBundle.TestObjectBundle())
+                .Remove<Scale>();
+
             Console.WriteLine("Entity 2:");
             Console.WriteLine(entity2.Get<ComponentBundle.Name>().Value);
             Console.WriteLine(entity2.Get<ComponentBundle.HP>().Value);
