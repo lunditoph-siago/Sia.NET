7f2a302aa32f366a5812cb8e0449a3fe9f1b999a
Phlam Sicusa <sicusa@gilatod.art>
2024-05-25 22:38:42 +0800

    refactor relation

--- Sia/Components/Relation.cs
+++ Sia/Components/Relation.cs
@@ -1,22 +1,54 @@
+using System.Runtime.CompilerServices;
+
 namespace Sia;
 
-public interface IRelation
+public interface IRelationComponent
 {
     public Identity Target { get; set; }
 }
 
-public interface IRelationTag;
+public interface IRelation;
+public interface IRelation<TArgument>;
 
-public record struct Relation<TTag>(Identity Target) : IRelation
-    where TTag : IRelationTag;
+public record struct Relation<TTag>(Identity Target) : IRelationComponent
+    where TTag : IRelation;
+
+public record struct Relation<TTag, TArgument>(Identity Target, TArgument Argument) : IRelationComponent
+    where TTag : IRelation<TArgument>;
 
 public static class RelationExtensions
 {
-    public static EntityRef GetTarget<TRelationTag>(this EntityRef entity, World world)
-        where TRelationTag : IRelationTag
-        => world[entity.Get<Relation<TRelationTag>>().Target];
+    public static EntityRef Add<TRelation>(this EntityRef entity, EntityRef target)
+        where TRelation : IRelation
+        => entity.Add(new Relation<TRelation>(target.Id));
 
-    public static EntityRef GetTarget<TRelationTag>(this EntityRef entity)
-        where TRelationTag : IRelationTag
-        => entity.GetTarget<TRelationTag>(World.Current);
+    public static EntityRef Add<TRelation, TArgument>(this EntityRef entity, Identity target, TArgument arg)
+        where TRelation : IRelation<TArgument>
+        => entity.Add(new Relation<TRelation, TArgument>(target, arg));
+
+    public static EntityRef Add<TRelation, TArgument>(this EntityRef entity, EntityRef target, TArgument arg)
+        where TRelation : IRelation<TArgument>
+        => entity.Add(new Relation<TRelation, TArgument>(target.Id, arg));
+
+    public static EntityRef Get<TRelation>(this EntityRef entity, World world)
+        where TRelation : IRelation
+        => world[entity.Get<Relation<TRelation>>().Target];
+
+    public static EntityRef Get<TRelation>(this EntityRef entity)
+        where TRelation : IRelation
+        => entity.Get<TRelation>(World.Current);
+
+    public static EntityRef Get<TRelation, TArgument>(
+        this EntityRef entity, World world, out TArgument arg)
+        where TRelation : IRelation<TArgument>
+    {
+        ref var relation = ref entity.Get<Relation<TRelation, TArgument>>();
+        arg = relation.Argument;
+        return world[relation.Target];
+    }
+
+    public static EntityRef Get<TRelation, TArgument>(
+        this EntityRef entity, out TArgument arg)
+        where TRelation : IRelation<TArgument>
+        => entity.Get<TRelation, TArgument>(World.Current, out arg);
 }
