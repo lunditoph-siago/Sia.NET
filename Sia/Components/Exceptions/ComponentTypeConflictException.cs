eed181a634c724add9c62f4010a017c957ab6aff
Phlam Sicusa <sicusa@gilatod.art>
2024-05-25 22:32:10 +0800

    refactor entity exception types

--- Sia/Components/Exceptions/ComponentTypeConflictException.cs
+++
@@ -1,9 +0,0 @@
-namespace Sia;
-
-[Serializable]
-public class ComponentTypeConflictException : EntityException
-{
-    public ComponentTypeConflictException() { }
-    public ComponentTypeConflictException(string message) : base(message) { }
-    public ComponentTypeConflictException(string message, Exception inner) : base(message, inner) { }
-}
