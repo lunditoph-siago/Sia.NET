ce8ab5426e45d551b9bc97e9d734b8c5ad391fc1
Phlam Sicusa <sicusa@gilatod.art>
2024-05-27 04:42:19 +0800

    refactor relation & fix serialization for argument relation

--- Sia/Components/Relation.cs
+++ Sia/Components/Relation.cs
@@ -5,21 +5,35 @@ public interface IRelationComponent
     public Entity Target { get; }
 }
 
-public interface IRelationComponent<TArgument> : IRelationComponent
+public interface IArgumentRelationComponent : IRelationComponent
 {
-    public object? BoxedArgument { get; }
+    public void HandleRelation(IRefGenericHandler handler);
 }
 
 public interface IRelation;
 public interface IRelation<TArgument>;
 
-public record struct Relation<TTag>(Entity Target) : IRelationComponent
-    where TTag : IRelation;
-
-public record struct Relation<TTag, TArgument>(Entity Target, TArgument Argument) : IRelationComponent<TArgument>
-    where TTag : IRelation<TArgument>
+public record struct Relation<TRelation>(Entity Target) : IRelationComponent
+    where TRelation : IRelation
 {
-    public readonly object? BoxedArgument => Argument;
+    public readonly override string ToString()
+        => typeof(TRelation) + "(" + Target + ")";
+}
+
+public record struct ArgRelation<TArgRelation>(Entity Target) : IArgumentRelationComponent
+    where TArgRelation : IRelation
+{
+    public TArgRelation Relation {
+        readonly get => _relation;
+        init => _relation = value;
+    }
+    private TArgRelation _relation;
+
+    public void HandleRelation(IRefGenericHandler handler)
+        => handler.Handle(ref _relation);
+
+    public readonly override string ToString()
+        => _relation + " (" + Target + ")";
 }
 
 public static class RelationExtensions
@@ -28,28 +42,28 @@ public static class RelationExtensions
         where TRelation : IRelation
         => entity.Add(new Relation<TRelation>(target));
 
-    public static Entity AddRelation<TRelation, TArgument>(this Entity entity, Entity target, TArgument arg)
-        where TRelation : IRelation<TArgument>
-        => entity.Add(new Relation<TRelation, TArgument>(target, arg));
+    public static Entity AddRelation<TArgRelation>(this Entity entity, TArgRelation relation, Entity target)
+        where TArgRelation : IRelation
+        => entity.Add(new ArgRelation<TArgRelation>(target) { Relation = relation });
 
     public static Entity SetRelation<TRelation>(this Entity entity, Entity target)
         where TRelation : IRelation
         => entity.Set(new Relation<TRelation>(target));
 
-    public static Entity SetRelation<TRelation, TArgument>(this Entity entity, Entity target, TArgument arg)
-        where TRelation : IRelation<TArgument>
-        => entity.Set(new Relation<TRelation, TArgument>(target, arg));
+    public static Entity SetRelation<TArgRelation>(this Entity entity, TArgRelation relation, Entity target)
+        where TArgRelation : IRelation
+        => entity.Set(new ArgRelation<TArgRelation>(target) { Relation = relation });
 
     public static Entity GetRelation<TRelation>(this Entity entity)
         where TRelation : IRelation
         => entity.Get<Relation<TRelation>>().Target;
 
-    public static Entity GetRelation<TRelation, TArgument>(
-        this Entity entity, out TArgument arg)
-        where TRelation : IRelation<TArgument>
+    public static Entity GetRelation<TArgRelation>(
+        this Entity entity, out TArgRelation arg)
+        where TArgRelation : IRelation
     {
-        ref var relation = ref entity.Get<Relation<TRelation, TArgument>>();
-        arg = relation.Argument;
+        ref var relation = ref entity.Get<ArgRelation<TArgRelation>>();
+        arg = relation.Relation;
         return relation.Target;
     }
 }
