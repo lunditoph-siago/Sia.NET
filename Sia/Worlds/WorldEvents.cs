2bde13774ac4d13d6d06242da2a9fc8ba7ab3ba9
Sieluna <seele.peng@gmail.com>
2024-10-28 20:01:55 +0200

    improve buffers robustness

--- Sia/Worlds/WorldEvents.cs
+++ Sia/Worlds/WorldEvents.cs
@@ -2,10 +2,10 @@ namespace Sia;
 
 public static class WorldEvents
 {
-    public class Add : SingletonEvent<Add> {}
-    public class Remove : SingletonEvent<Remove> {}
+    public class Add : SingletonEvent<Add>;
+    public class Remove : SingletonEvent<Remove>;
 
-    public class Add<TComponent> : SingletonEvent<Add<TComponent>> {}
-    public class Set<TComponent> : SingletonEvent<Set<TComponent>> {}
-    public class Remove<TComponent> : SingletonEvent<Remove<TComponent>> {}
+    public class Add<TComponent> : SingletonEvent<Add<TComponent>>;
+    public class Set<TComponent> : SingletonEvent<Set<TComponent>>;
+    public class Remove<TComponent> : SingletonEvent<Remove<TComponent>>;
 }
