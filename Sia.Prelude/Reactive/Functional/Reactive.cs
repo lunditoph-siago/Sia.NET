f8f57a6bc11a8eab986e9ee8fcfe5aeb537f5660
Sieluna <seele.peng@gmail.com>
2026-07-19 22:32:00 +0300

    add static reactive factories and remove hand-written group

---
+++ Sia.Prelude/Reactive/Functional/Reactive.cs
@@ -0,0 +1,219 @@
+namespace Sia.Reactive;
+
+using System.Runtime.CompilerServices;
+
+public readonly record struct ReactiveKeyed<TKey, TValue>(
+    TKey Key,
+    TValue Value)
+    where TKey : notnull;
+
+public delegate ReactiveNode<TTerm> ReactiveItemRenderer<TItem, TTerm>(
+    scoped in TItem item)
+    where TTerm : struct, ITerm<TTerm>;
+
+public readonly record struct ReactiveItemSpec<TItem, TTerm>(
+    TItem Item,
+    ReactiveItemRenderer<TItem, TTerm> Render)
+    : ISpec<ReactiveItemSpec<TItem, TTerm>, Unit, TTerm>
+    where TTerm : struct, ITerm<TTerm>
+{
+    public static TTerm Expand(
+        in ReactiveItemSpec<TItem, TTerm> props,
+        in Unit state,
+        in ExpandContext context)
+        => props.Render(props.Item).Term;
+}
+
+public delegate TMessage ReactiveEventHandler<TEvent, TMessage>(
+    scoped in TEvent @event)
+    where TEvent : IEvent;
+
+public delegate TResource ReactiveEffectSetup<TDependencies, TResource>(
+    scoped in TDependencies dependencies)
+    where TDependencies : struct, IEquatable<TDependencies>;
+
+public delegate void ReactiveEffectCleanup<TResource>(
+    scoped in TResource resource);
+
+public delegate ReactiveNode<TTerm> ReactiveContextRenderer<TContext, TTerm>(
+    scoped in TContext value)
+    where TContext : struct
+    where TTerm : struct, ITerm<TTerm>;
+
+public static partial class Reactive
+{
+    public static ReactiveNode<UnitTerm> None => new(default);
+
+    public static ReactiveComponent<TProps, TState, TMessage> Define<
+        TProps, TState, TMessage>(
+        ReactiveInitial<TProps, TState> initialState,
+        ReactiveReducer<TState, TMessage> reduce,
+        ReactiveRenderer<TProps, TState> render)
+        where TProps : struct
+        where TState : struct
+        => new(initialState, reduce, render);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReactiveNode<EntityTerm<TList, UnitTerm>> Entity<TList>(
+        scoped in TList components)
+        where TList : struct, IHList
+        => new(Term.Entity(components));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReactiveNode<EntityTerm<TList, TChildren>> Entity<
+        TList, TChildren>(
+        scoped in TList components,
+        scoped in ReactiveNode<TChildren> children)
+        where TList : struct, IHList
+        where TChildren : struct, ITerm<TChildren>
+        => new(Term.Entity(components, children.Term));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReactiveNode<CondTerm<TTerm>> When<TTerm>(
+        bool condition,
+        scoped in ReactiveNode<TTerm> node)
+        where TTerm : struct, ITerm<TTerm>
+        => new(Term.Cond(condition, node.Term));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReactiveNode<EitherTerm<TFirst, TSecond>> Either<
+        TFirst, TSecond>(
+        bool first,
+        scoped in ReactiveNode<TFirst> whenTrue,
+        scoped in ReactiveNode<TSecond> whenFalse)
+        where TFirst : struct, ITerm<TFirst>
+        where TSecond : struct, ITerm<TSecond>
+        => new(Term.Either(first, whenTrue.Term, whenFalse.Term));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReactiveNode<ScopeTerm<TContext, TChildren>> Provide<
+        TContext, TChildren>(
+        scoped in TContext value,
+        scoped in ReactiveNode<TChildren> children)
+        where TContext : struct
+        where TChildren : struct, ITerm<TChildren>
+        => new(Term.Scope(value, children.Term));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReactiveNode<LiftTerm<ContextRenderSpec<TContext, TTerm>>>
+        Use<TContext, TTerm>(
+            ReactiveContextRenderer<TContext, TTerm> render)
+        where TContext : struct
+        where TTerm : struct, ITerm<TTerm>
+        => new(Term.Lift(new ContextRenderSpec<TContext, TTerm>(
+            render ?? throw new ArgumentNullException(nameof(render)))));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReactiveKeyed<TKey, TValue> Keyed<TKey, TValue>(
+        TKey key,
+        scoped in TValue value)
+        where TKey : notnull
+        => new(key, value);
+
+    public static ReactiveNode<ForEachTerm<TKey, ReactiveNodeSpec<TTerm>>>
+        ForEach<TKey, TTerm>(
+            ReadOnlyMemory<ReactiveKeyed<TKey, ReactiveNode<TTerm>>> items)
+        where TKey : notnull
+        where TTerm : struct, ITerm<TTerm>
+    {
+        var source = items.Span;
+        var keyed = new Keyed<TKey, ReactiveNodeSpec<TTerm>>[source.Length];
+        for (var index = 0; index < source.Length; index++) {
+            keyed[index] = Term.Keyed(
+                source[index].Key,
+                new ReactiveNodeSpec<TTerm>(source[index].Value.Term));
+        }
+        return new(Term.ForEach<TKey, ReactiveNodeSpec<TTerm>>(keyed));
+    }
+
+    public static ReactiveNode<ForEachTerm<TKey, ReactiveNodeSpec<TTerm>>>
+        ForEach<TKey, TTerm>(
+            params ReactiveKeyed<TKey, ReactiveNode<TTerm>>[] items)
+        where TKey : notnull
+        where TTerm : struct, ITerm<TTerm>
+    {
+        ArgumentNullException.ThrowIfNull(items);
+        return ForEach<TKey, TTerm>(items.AsMemory());
+    }
+
+    public static ReactiveNode<ForEachTerm<
+        TKey, ReactiveItemSpec<TItem, TTerm>>> ForEach<
+        TKey, TItem, TTerm>(
+        ReadOnlyMemory<ReactiveKeyed<TKey, TItem>> items,
+        ReactiveItemRenderer<TItem, TTerm> render)
+        where TKey : notnull
+        where TTerm : struct, ITerm<TTerm>
+    {
+        ArgumentNullException.ThrowIfNull(render);
+        var source = items.Span;
+        var keyed = new Keyed<
+            TKey, ReactiveItemSpec<TItem, TTerm>>[source.Length];
+        for (var index = 0; index < source.Length; index++) {
+            keyed[index] = Term.Keyed(
+                source[index].Key,
+                new ReactiveItemSpec<TItem, TTerm>(
+                    source[index].Value,
+                    render));
+        }
+        return new(Term.ForEach<
+            TKey, ReactiveItemSpec<TItem, TTerm>>(keyed));
+    }
+
+    public static ReactiveNode<ForEachTerm<
+        TKey, ReactiveItemSpec<TItem, TTerm>>> ForEach<
+        TKey, TItem, TTerm>(
+        ReactiveItemRenderer<TItem, TTerm> render,
+        params ReactiveKeyed<TKey, TItem>[] items)
+        where TKey : notnull
+        where TTerm : struct, ITerm<TTerm>
+    {
+        ArgumentNullException.ThrowIfNull(items);
+        return ForEach<TKey, TItem, TTerm>(items.AsMemory(), render);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReactiveNode<FunctionalComponentTerm<
+        TProps, TState, TMessage>> Component<TProps, TState, TMessage>(
+        ReactiveComponent<TProps, TState, TMessage> component,
+        scoped in TProps props)
+        where TProps : struct
+        where TState : struct
+    {
+        ArgumentNullException.ThrowIfNull(component);
+        return new(new(component, props));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReactiveNode<ScheduleTerm<TLabel, TChildren>> Schedule<
+        TLabel, TChildren>(
+        TLabel _,
+        scoped in ReactiveNode<TChildren> children)
+        where TLabel : struct
+        where TChildren : struct, ITerm<TChildren>
+        => new(Term.Schedule(default(TLabel), children.Term));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReactiveNode<SystemTerm<TSystem>> System<TSystem>()
+        where TSystem : ISystem, new()
+        => new(Term.System<TSystem>());
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReactiveNode<EventBindingTerm<TEvent, TMessage>> On<
+        TEvent, TMessage>(
+        ReactiveEventHandler<TEvent, TMessage> handler)
+        where TEvent : IEvent
+        => new(new(handler
+            ?? throw new ArgumentNullException(nameof(handler))));
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReactiveNode<DeferredEffectTerm<
+        TDependencies, TResource>> Effect<TDependencies, TResource>(
+        scoped in TDependencies dependencies,
+        ReactiveEffectSetup<TDependencies, TResource> setup,
+        ReactiveEffectCleanup<TResource> cleanup)
+        where TDependencies : struct, IEquatable<TDependencies>
+        => new(new(
+            dependencies,
+            setup ?? throw new ArgumentNullException(nameof(setup)),
+            cleanup ?? throw new ArgumentNullException(nameof(cleanup))));
+}
--- Sia.Prelude/Reactive/Terms/Term.cs
+++ Sia.Prelude/Reactive/Terms/Term.cs
@@ -2,7 +2,7 @@ namespace Sia.Reactive;
 
 using System.Runtime.CompilerServices;
 
-public static class Term
+public static partial class Term
 {
     public static UnitTerm None => default;
 
@@ -36,9 +36,9 @@ public static class Term
         => new(children);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ScopeTerm<TCtx, TChildren> Scope<TCtx, TChildren>(
-        in TCtx value, in TChildren children)
-        where TCtx : struct
+    public static ScopeTerm<TContext, TChildren> Scope<TContext, TChildren>(
+        in TContext value, in TChildren children)
+        where TContext : struct
         where TChildren : struct, ITerm<TChildren>
         => new(value, children);
 
@@ -71,75 +71,4 @@ public static class Term
         where TFirst : struct, ITerm<TFirst>
         where TSecond : struct, ITerm<TSecond>
         => new(isFirst, first, second);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Group<T1, T2> Group<T1, T2>(in T1 item1, in T2 item2)
-        where T1 : struct, ITerm<T1>
-        where T2 : struct, ITerm<T2>
-        => new(item1, item2);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Group<T1, T2, T3> Group<T1, T2, T3>(
-        in T1 item1, in T2 item2, in T3 item3)
-        where T1 : struct, ITerm<T1>
-        where T2 : struct, ITerm<T2>
-        where T3 : struct, ITerm<T3>
-        => new(item1, item2, item3);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Group<T1, T2, T3, T4> Group<T1, T2, T3, T4>(
-        in T1 item1, in T2 item2, in T3 item3, in T4 item4)
-        where T1 : struct, ITerm<T1>
-        where T2 : struct, ITerm<T2>
-        where T3 : struct, ITerm<T3>
-        where T4 : struct, ITerm<T4>
-        => new(item1, item2, item3, item4);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Group<T1, T2, T3, T4, T5> Group<T1, T2, T3, T4, T5>(
-        in T1 item1, in T2 item2, in T3 item3, in T4 item4, in T5 item5)
-        where T1 : struct, ITerm<T1>
-        where T2 : struct, ITerm<T2>
-        where T3 : struct, ITerm<T3>
-        where T4 : struct, ITerm<T4>
-        where T5 : struct, ITerm<T5>
-        => new(item1, item2, item3, item4, item5);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Group<T1, T2, T3, T4, T5, T6> Group<T1, T2, T3, T4, T5, T6>(
-        in T1 item1, in T2 item2, in T3 item3, in T4 item4, in T5 item5, in T6 item6)
-        where T1 : struct, ITerm<T1>
-        where T2 : struct, ITerm<T2>
-        where T3 : struct, ITerm<T3>
-        where T4 : struct, ITerm<T4>
-        where T5 : struct, ITerm<T5>
-        where T6 : struct, ITerm<T6>
-        => new(item1, item2, item3, item4, item5, item6);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Group<T1, T2, T3, T4, T5, T6, T7> Group<T1, T2, T3, T4, T5, T6, T7>(
-        in T1 item1, in T2 item2, in T3 item3, in T4 item4, in T5 item5, in T6 item6,
-        in T7 item7)
-        where T1 : struct, ITerm<T1>
-        where T2 : struct, ITerm<T2>
-        where T3 : struct, ITerm<T3>
-        where T4 : struct, ITerm<T4>
-        where T5 : struct, ITerm<T5>
-        where T6 : struct, ITerm<T6>
-        where T7 : struct, ITerm<T7>
-        => new(item1, item2, item3, item4, item5, item6, item7);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Group<T1, T2, T3, T4, T5, T6, T7, T8> Group<T1, T2, T3, T4, T5, T6, T7, T8>(
-        in T1 item1, in T2 item2, in T3 item3, in T4 item4, in T5 item5, in T6 item6,
-        in T7 item7, in T8 item8)
-        where T1 : struct, ITerm<T1>
-        where T2 : struct, ITerm<T2>
-        where T3 : struct, ITerm<T3>
-        where T4 : struct, ITerm<T4>
-        where T5 : struct, ITerm<T5>
-        where T6 : struct, ITerm<T6>
-        where T7 : struct, ITerm<T7>
-        where T8 : struct, ITerm<T8>
-        => new(item1, item2, item3, item4, item5, item6, item7, item8);
 }
