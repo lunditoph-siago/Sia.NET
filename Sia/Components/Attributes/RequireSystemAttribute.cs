6bdcb564e68a98e511d81590c6f044a5a97f7cc1
Sieluna <seele.peng@gmail.com>
2026-07-26 13:16:50 +0300

    remove unused types and zero-use parallel iteration variants

--- Sia/Components/Attributes/RequireSystemAttribute.cs
+++
@@ -1,5 +0,0 @@
-namespace Sia;
-
-[AttributeUsage(AttributeTargets.Struct, Inherited = false, AllowMultiple = true)]
-public sealed class RequireSystemAttribute<TSystem> : Attribute
-    where TSystem : ISystem;
